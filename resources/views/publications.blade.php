40d301715698f591dbbba2fa456ca731e0b82482
themaison <melnichuk1712@mail.ru>
2025-02-25 21:39:15 +0300

    books design

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -5,48 +5,148 @@
 
 @section('content')
     <section class="catalog">
-        <div class="container">
+        <div class="library-header">
 
-            <div class="library-header">
-
-                <div class="search-controller">
-                    <div class="catalog-icon">
-                        <img src="{{ asset('images/icons/library.svg') }}" alt="Catalog Icon">
-                    </div>
-
-                    <h2>Каталог</h2>
-
-                    <div class="library-search">
-                        <input type="text" class="search-input" placeholder="Поиск по каталогу...">
-                        {{--                <button class="search-button">Поиск</button>--}}
-                    </div>
-
-                    <!-- Чекбоксы для сортировки -->
-                    <div class="catalog-sort">
-                        <label><input type="checkbox" class="sort-checkbox" /> Книги</label>
-                        <label><input type="checkbox" class="sort-checkbox" /> Комиксы</label>
-                    </div>
+            <div class="search-controller">
+                <div class="catalog-icon">
+                    <img src="{{ asset('images/icons/library.svg') }}" alt="Catalog Icon">
                 </div>
 
-                <div class="category-controller">
-                    <!-- Блок фильтров по категориям -->
-                    <div class="filters">
-                        <span class="filter-tag">Приключения</span>
-                        <span class="filter-tag">Научная фантастика</span>
-                        <span class="filter-tag">Фэнтези</span>
-                        <span class="filter-tag">Мистика</span>
-                    </div>
-{{--                    <div class="controll-category">--}}
-{{--                        --}}
-{{--                    </div>--}}
+                <h2>Каталог</h2>
+
+                <div class="library-search">
+                    <input type="text" class="search-input" placeholder="Поиск по каталогу...">
                 </div>
 
+                <!-- Чекбоксы для сортировки -->
+                <div class="catalog-sort">
+                    <label><input type="checkbox" class="sort-checkbox text-small" /> Книги</label>
+                    <label><input type="checkbox" class="sort-checkbox text-small" /> Комиксы</label>
+                </div>
             </div>
 
-            <div class="library-content">
-                <p>В скором времени мы добавим товары и другие элементы каталога.</p>
+            <div class="filters">
+                <span class="filter-tag text-medium choice">Приключения</span>
+                <span class="filter-tag text-medium">Научная фантастика</span>
+                <span class="filter-tag text-medium">Фэнтези</span>
+                <span class="filter-tag text-medium">Мистика</span>
             </div>
 
+        </div>
+
+        <div class="library-content">
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
+            <div class="book">
+                <img src="{{ asset('images/comics_cover.png') }}" alt="" class="comics_cover">
+                <div class="description">
+                    <div class="categories">
+                        <div class="category">Хоррор</div>
+                        <div class="category">Ужасы</div>
+                        <div class="category">Триллер</div>
+                    </div>
+                    <div class="about">
+                        <p class="title text-big">BLACK MIRROR</p>
+                        <p class="author text-small">Мухва</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
