d65a68a8f7a340170a9be5f78cd36e3bec590147
de5peratee <pirat-29_29@mail.ru>
2025-04-23 15:54:00 +0300

    роутинг и контроллеры для каталога

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
     public function index()
     {
-        
+        $catalogs = Catalog::with('genres')->paginate(20);
+        return view('catalog.index', compact('catalogs'));
+    }
+
+    public function show($id)
+    {
+        $catalog = Catalog::with('genres')->findOrFail($id);
+        return view('catalog.show', compact('catalog'));
     }
 
 
