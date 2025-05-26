09f577c5d67e79b5d20f444c86babe9c87faafee
de5peratee <pirat-29_29@mail.ru>
2025-05-26 18:02:52 +0300

    moonshine

---
+++ app/MoonShine/Resources/UsersResource.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\Support\DTOs\FileItemExtra;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Components\Layout\Column;
+use MoonShine\UI\Components\Layout\Grid;
+use MoonShine\UI\Components\Layout\LineBreak;
+use MoonShine\UI\Fields\Date;
+use MoonShine\UI\Fields\Email;
+use MoonShine\UI\Fields\ID;
+use MoonShine\UI\Fields\Image;
+use MoonShine\UI\Fields\Password;
+use MoonShine\UI\Fields\PasswordRepeat;
+use MoonShine\UI\Fields\Select;
+use MoonShine\UI\Fields\Text;
+use MoonShine\UI\Fields\Textarea;
+
+
+class UsersResource extends ModelResource
+{
+    protected string $model = User::class;
+
+    protected string $title = 'Пользователи';
+
+
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Никнейм', 'nickname'),
+            Email::make('Email', 'email'),
+            Text::make('Имя', 'name'),
+            Text::make('Фамилия', 'last_name'),
+        ];
+    }
+
+
+    protected function formFields(): iterable
+    {
+        return [
+            Grid::make([
+                Column::make([
+                    Box::make('Общая информация', [
+                        ID::make()->sortable(),
+
+                        Text::make('Никнейм', 'nickname')->required(),
+                        Email::make('Email', 'email')->required(),
+
+                        Text::make('Имя', 'name')->required(),
+                        Text::make('Фамилия', 'last_name')->required(),
+                        Date::make('Дата рождения', 'birth_date')->required(),
+
+                        Textarea::make('Описание', 'about')->nullable(),
+
+                        Image::make('Иконка', 'icon')
+                            ->disk('public')
+                            ->extraAttributes(fn(string $filename, int $index): ?FileItemExtra => new FileItemExtra(wide: false, auto: true, styles: 'width: 250px;'))
+                            ->dir('icon_user')
+                            ->nullable(),
+
+                        Image::make('Шапка профиля', 'head_profile')
+                            ->dir('head_profile')
+                            ->disk('public')
+                            ->extraAttributes(fn(string $filename, int $index): ?FileItemExtra => new FileItemExtra(wide: false, auto: true, styles: 'width: 250px;'))
+                            ->nullable(),
+
+                        Select::make('Роль', 'role')
+                            ->options([
+                                'user' => 'Пользователь',
+                                'editor' => 'Редактор',
+                            ])
+
+                    ]),
+
+                    LineBreak::make(),
+
+                    Box::make('Пароль', [
+
+                        Password::make('Пароль', 'password')
+                            ->customAttributes(['autocomplete' => 'new-password']),
+                        PasswordRepeat::make('Повторите пароль')
+                            ->customAttributes(['autocomplete' => 'confirm-password'])
+                    ]),
+                ]),
+            ]),
+        ];
+    }
+
+    protected function detailFields(): iterable
+    {
+        return [
+            ID::make(),
+            Text::make('Никнейм', 'nickname'),
+            Email::make('Email', 'email'),
+            Text::make('Имя', 'name'),
+            Text::make('Фамилия', 'last_name'),
+            Date::make('Дата рождения', 'birth_date'),
+            Textarea::make('Описание', 'about'),
+            Image::make('Иконка', 'icon'),
+            Image::make('Шапка профиля', 'head_profile'),
+            Text::make('Роль', 'role'),
+        ];
+    }
+
+    /**
+     * @param User $item
+     * @return array<string, string[]|string>
+     */
+    protected function rules(mixed $item): array
+    {
+        return [
+            'nickname' => ['required', 'string', 'min:5', 'max:20', 'regex:/^[a-zA-Z0-9_-]+$/', 'unique:users,nickname,' . $item->id],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $item->id],
+            'name' => ['required', 'string', 'max:100', 'regex:/^[a-zA-Zа-яА-ЯёЁ]+$/u'],
+            'last_name' => ['required', 'string', 'max:100', 'regex:/^[a-zA-Zа-яА-ЯёЁ]+$/u'],
+            'birth_date' => ['required', 'date'],
+            'password' => !$item->exists
+                ? 'required|min:8|required_with:password_repeat|same:password_repeat'
+                : 'sometimes|nullable|min:8|required_with:password_repeat|same:password_repeat',
+        ];
+    }
+}
