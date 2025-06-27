6b9c646d6cd69f10e953d2582b90a11721b5a48e
de5peratee <pirat-29_29@mail.ru>
2025-06-27 14:29:13 +0300

    фиксы

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="description" content="@yield('meta_description', 'Литкомс — первый комикс-центр Севастополя. Комикс-школа, клуб, библиотека. Бесплатные занятия по средам и воскресеньям.')">
     <title>@yield('title', 'Шаблон')</title>
 
     @vite(['resources/css/app.css'])
